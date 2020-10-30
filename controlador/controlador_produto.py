5466dbf69e24d0e0b43b7df9bd80b9791e60c0c2
Francielle Lopes <franciellelps@gmail.com>
2020-10-30 18:22:23 +0000

    Criação da TelaProduto ControladorProduto e Produto

---
+++ controlador/controlador_produto.py
@@ -0,0 +1,62 @@
+from tela.tela_produto import TelaProduto
+from entidade.produto import Produto
+
+class ControladorProduto:
+
+  def __init__(self):
+    self.__produtos = []
+    self.__tela_produto = TelaProduto(self)
+    self.__tela_exibindo = True
+
+  def inicia(self):
+    self.abre_tela_inicial()
+
+  def adiciona(self):
+    dados = self.__tela_produto.requisita_dados_cadastro()
+    novo_produto = Produto(dados["codigo"],["nome"],["valor"],["quantidade"])
+    self.__produtos.append(novo_produto)
+
+  def remove(self):
+    codigo = self.__tela_produto.requisita_dados_remover()
+    for produto in self.__produtos:
+      if produto.codigo == codigo:
+        produto_remover = (produto)
+        self.__produtos.remove(produto_remover)
+        break
+
+  def atualiza(self):
+    codigo = self.__tela_produto.requisita_dado_atualizar()
+    for produto in self.__produtos:
+      if produto.codigo == codigo:
+        dados = self.__tela_produto.atualiza_produto()
+        produto.nome = dados.nome
+        produto.valor = dados.valor
+        produto.quantidade = dados.quantidade
+
+  def lista(self):
+    for produto in self.__produtos:
+      self.__tela_produto.mostra_dados_cadastrados(produto.codigo, produto.nome, produto.valor, produto.quantidade)
+
+  def abre_tela_inicial(self):
+    opcoes = {1: self.adiciona,2: self.remove,3: self.atualiza,4: self.lista,5: self.imprime_relatorio,0: self.finaliza_tela}
+    
+    while self.__tela_exibindo:
+      opcao = self.__tela_produto.mostra_opcoes()
+      funcao = opcoes[opcao]
+      funcao()
+ 
+  def finaliza_tela(self):
+    self.__tela_exibindo = False
+
+  def relatorio_estoque(self):
+    pass
+    #toda vez que um produto e cadastrado, ele entra no relatorio
+
+  def atualiza_estoque(self):
+    pass
+    #toda vez que uma venda ocorrer, deve substrair do estoque a quantidade do produto vendido
+    
+  def imprime_relatorio(self):
+    pass
+    #imprimir o relatorio de estoque
+  
---
+++ tela/tela_produto.py
@@ -0,0 +1,47 @@
+class TelaProduto:
+    def __init__(self, controlador_produto):
+      self.__controle = controlador_produto
+
+    def mostra_opcoes(self): 
+      print("------ PRODUTO ------")
+      print("1 - Adicionar Produto")
+      print("2 - Remover Produto")
+      print("3 - Atualizar Produto")
+      print("4 - Listar Produtos")
+      print("5 - Imprimir Relatório de Estoque")
+      print("0 - Voltar")
+    
+      opcao = int(input("Escolha a opção: "))
+      return opcao
+
+    def requisita_dados_cadastro(self):
+      print("------ CADASTRAR PRODUTO------")
+      codigo = int(input("Codigo do produto: "))
+      nome = input("Nome do produto: ")
+      valor = float(input("Valor do produto: "))
+      quantidade = int(input("Quantidade do produto: "))
+      return {"codigo": codigo, "nome": nome, "valor": valor, "quantidade": quantidade}
+
+    def mostra_dados_cadastrados(self, codigo: int, nome: str, valor: float, quantidade: int):
+      print("Codigo: ", str(codigo))
+      print("Nome: ", nome)
+      print("Valor: ", str(valor))
+      print("Quantidade: ", str(quantidade))
+
+    def requisita_dado_remover(self):
+      print("------REMOVER PRODUTO------")
+      codigo = int(input("Digite o codigo do produto que deseja remover: "))
+      return codigo
+
+    def requisita_dado_atualizar(self):
+      print("------ATUALIZAR PRODUTO------")
+      codigo = int(input("Digite o codigo do produto que deseja atualizar: "))
+      return codigo
+    def atualiza_produto(self):
+      nome = input("Digite o novo nome: ")
+      valor = float(input("Digite o novo valor: "))
+      quantidade = int(input("Digite a nova quantidade: "))
+      return {"nome": nome, "valor": valor, "quantidade": quantidade}
+
+    def avisos(self):
+      pass
